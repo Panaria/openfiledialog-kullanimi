6cc590a6512049f19d819ce3947d973da052e1c5
Panaria <baranyldzby@gmail.com>
2023-10-23 19:52:44 +0300

    Add project files.

---
+++ openfiledialog kullanimi/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace openfiledialog_kullanimi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+
+            pictureBox1.ImageLocation = openFileDialog1.FileName;
+        }
+    }
+}
